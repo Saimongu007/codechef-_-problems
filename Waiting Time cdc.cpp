a06a573a4171e677ed0c24ed698d55e696bb1dec
Saimongu Marma <124234753+Saimongu007@users.noreply.github.com>
2023-09-26 00:29:53 +0600

    Find the number of remaining days

---
+++ Waiting Time cdc.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int t;
+	cin>> t;
+
+	while(t--)
+	{
+	    int x, k;
+	    cin>> k >> x;
+
+	    cout<<(7*k)-x<<endl;
+	}
+	return 0;
+}
+
