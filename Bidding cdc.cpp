1e1f30f831d12e8c0b99a00dff4992304436d900
Saimongu Marma <124234753+Saimongu007@users.noreply.github.com>
2023-09-26 00:33:58 +0600

    Determine who will win the auction.

---
+++ Bidding cdc.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int t;
+	cin>> t;
+
+	while(t--)
+	{
+	    int A, B, C;
+	    cin>> A >> B>> C;
+
+	    if(A > B && A > C)
+	    {
+	        cout<<"Alice" <<endl;
+	    }
+	    else if(B > A && B > C)
+	    {
+	        cout<<"Bob" <<endl;
+	    }
+	    else
+	    {
+	        cout<<"Charlie"<<endl;
+	    }
+	}
+	return 0;
+}
+
