e309d9fa4ebac13819f881e7e5cea7d9744ae82f
Saimongu Marma <124234753+Saimongu007@users.noreply.github.com>
2023-11-19 23:34:30 +0600

    minimum total cost the group of is able toChef-TV.

---
+++ Subscriptions-cdc.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int n, x;
+        cin >> n >> x;
+
+        if (n % 6 == 0)
+        {
+            cout << (n * x) / 6 << endl;
+        }
+        else
+        {
+            cout << (n / 6 + 1) * x << endl;
+        }
+    }
+    return 0;
+}
