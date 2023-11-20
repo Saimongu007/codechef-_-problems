c9df7bcbed8bdb731d260831de4d526a98fded2b
Saimongu Marma <124234753+Saimongu007@users.noreply.github.com>
2023-11-20 21:23:10 +0600

    possible 2numbers out of 3number their sum is odd.

---
+++ Odd-Sum-Pair-cdc.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int a, b, c;
+        cin >> a >> b >> c;
+
+        if ((a + b) % 2 != 0 || (b + c) % 2 != 0 || (c + a) % 2 != 0)
+        {
+            cout << "YES" << endl;
+        }
+        else
+        {
+            cout << "NO" << endl;
+        }
+    }
+    return 0;
+}
