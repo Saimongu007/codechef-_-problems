5b90bf7c33aaee80a9414fd3acd564db2dc7c0c2
Saimongu Marma <124234753+Saimongu007@users.noreply.github.com>
2023-11-20 21:23:43 +0600

    able to complete all assignments on time or not?

---
+++ Janmansh-and-Assignments-cdc.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int x;
+        cin >> x;
+
+        if ((x + 3) <= 10)
+        {
+            cout << "Yes" << endl;
+        }
+        else
+        {
+            cout << "No" << endl;
+        }
+    }
+    return 0;
+}
