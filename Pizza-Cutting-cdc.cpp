c0ec619aa193853e3f4ff8855cf020247edaa534
Saimongu Marma <124234753+Saimongu007@users.noreply.github.com>
2023-11-21 22:14:49 +0600

    Divide the pizza (possibly zero) number of cuts.

---
+++ Pizza-Cutting-cdc.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int n;
+        cin >> n;
+
+        if (n % 2 == 0 || n == 1)
+        {
+            cout << "YES" << endl;
+        }
+        else
+        {
+            cout << "NO" << endl;
+        }
+    }
+    return 0;
+}
