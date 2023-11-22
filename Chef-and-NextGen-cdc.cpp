7d7536162e8a1a8146461a4551d7ddbd664ebe33
Saimongu Marma <124234753+Saimongu007@users.noreply.github.com>
2023-11-22 20:09:13 +0600

    determine  the moon can provide units of power.

---
+++ Chef-and-NextGen-cdc.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int a, b, x, y;
+        cin >> a >> b >> x >> y;
+
+        int need = a * b;
+        int m = x * y;
+
+        if (need <= m)
+        {
+            cout << "Yes" << endl;
+        }
+        else
+        {
+            cout << "No" << endl;
+        }
+    }
+    return 0;
+}
