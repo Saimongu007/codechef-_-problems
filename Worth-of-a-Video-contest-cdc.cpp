3832a8a335e7472c2d9dab34aecccff0db4551e3
Saimongu Marma <124234753+Saimongu007@users.noreply.github.com>
2023-11-23 22:14:50 +0600

    how many words is this video worth in total?

---
+++ Worth-of-a-Video-contest-cdc.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>> t;
+	
+	while(t--)
+	{
+	    int s;
+	    cin>> s;
+	    
+	    int p = s * 24;
+	    int w = p * 1000;
+	    
+	    cout<< w <<endl;
+	}
+	return 0;
+}
