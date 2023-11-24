2e469bb4d1df3807ea32cb5414c0e3852d87b357
Saimongu Marma <124234753+Saimongu007@users.noreply.github.com>
2023-11-25 00:37:26 +0600

    Chef has three water bottles.

---
+++ Water-Filling-cdc.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int b1, b2, b3;
+        cin >> b1 >> b2 >> b3;
+
+        // if((b1== 0 || b2 == 0 || b3== 0) && ((b1||b2) == 0) && ((b2||b3) == 0) && ((b3||b1)==0))
+        if ((b1 == 0 && b2 == 0) || (b2 == 0 && b3 == 0) || (b3 == 0 && b1 == 0))
+        {
+            cout << "Water filling time" << endl;
+        }
+        else
+        {
+            cout << "Not now" << endl;
+        }
+    }
+    return 0;
+}
