bc6056be248065c1f49b822230c4a6e762d4b678
Saimongu Marma <124234753+Saimongu007@users.noreply.github.com>
2023-11-29 00:38:26 +0600

    find the number of eligible voters.

---
+++ Elections-in-Chefland-cdc.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int n, x;
+        cin >> n >> x;
+        int count = 0;
+
+        int a[n];
+
+        for (int i = 0; i < n; i++)
+        {
+            cin >> a[i];
+        }
+
+        for (int i = 0; i < n; i++)
+        {
+            if (a[i] >= x)
+            {
+                count++;
+            }
+        }
+        cout << count << endl;
+    }
+    return 0;
+}
