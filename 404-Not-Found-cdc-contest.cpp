3d11b56d555197375b7f1f3c6db4c2c1d7e0125f
Saimongu Marma <124234753+Saimongu007@users.noreply.github.com>
2023-12-07 01:34:20 +0600

    determine the website response.

---
+++ 404-Not-Found-cdc-contest.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int X;
+	cin>> X;
+	
+	if( X == 404)
+	{
+	    cout<<"NOT FOUND"<<endl;
+	}
+	else
+	{
+	    cout<<"FOUND"<<endl;
+	}
+	return 0;
+}
