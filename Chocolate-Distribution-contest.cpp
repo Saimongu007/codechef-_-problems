96aef82e534544b3e4adbe8d6ba38021ee8d9398
Saimongu Marma <124234753+Saimongu007@users.noreply.github.com>
2024-01-04 23:40:43 +0600

    minimum and maximum number of possible children

---
+++ Chocolate-Distribution-contest.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int n;
+        cin >> n;
+
+        int min = (n + 1) / 2;
+        int max = n;
+
+        cout << min << " " << max << endl;
+    }
+    return 0;
+}
