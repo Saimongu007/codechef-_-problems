2480cc6e12c7fb175de29a96606b84e5f307dc49
Saimongu Marma <124234753+Saimongu007@users.noreply.github.com>
2024-01-04 23:39:22 +0600

    Whether there is a Codechef round today?

---
+++ Codechef-Round-contest.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int N;
+    cin >> N;
+
+    if (N == 4)
+    {
+        cout << "YES" << endl;
+    }
+    else
+    {
+        cout << "NO" << endl;
+    }
+    return 0;
+}
