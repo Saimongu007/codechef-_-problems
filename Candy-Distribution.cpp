26a9476e8cd12bb0cc1e1aeed3d1842af4ac2fe3
Saimongu Marma <124234753+Saimongu007@users.noreply.github.com>
2024-01-19 13:28:59 +0600

    Determine whether it is possible to do so.

---
+++ Candy-Distribution.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int x, y;
+        cin >> x >> y;
+
+        int z = x / y;
+        if (z % 2 == 0 && x % y == 0)
+        {
+            cout << "YES" << endl;
+        }
+        else
+        {
+            cout << "NO" << endl;
+        }
+    }
+    return 0;
+}
