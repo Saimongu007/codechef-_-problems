48c34792bd227e8ccf748438be4942fabd1a9f40
Saimongu Marma <124234753+Saimongu007@users.noreply.github.com>
2024-01-31 22:39:19 +0600

    START119D/problems/ADVITIYA1

---
+++ Advitiya.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    if (n >= 16 && n <= 18)
+    {
+        cout << "ADVITIYA" << endl;
+    }
+    else
+    {
+        cout << "WAITING FOR ADVITIYA" << endl;
+    }
+
+    return 0;
+}
